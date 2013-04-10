55250f1a96f8bbb9ad3acc28aa886bcf71064bdd
Matt Heath <matt@mattheath.com>
2013-04-10 10:26:25 +0100

    Initial licence template & manifest

---
+++ manifests/license.pp
@@ -0,0 +1,15 @@
+# license_name: Your Name
+# license_key: 7ad6c7a6c87...
+
+class charles::license(
+  $license_key  = undef,
+  $license_name = undef
+) {
+
+  include boxen::config
+
+  file { "/Users/${::boxen_user}/Library/Preferences/com.xk72.charles.config":
+    content => template('charles/com.xk72.charles.config.erb'),
+  }
+
+}
